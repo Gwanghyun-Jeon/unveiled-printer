2800f1effe6f3b1be8c4861bb9e384c2f5c9fe00
GH <gwanghyunjeon@gmail.com>
2023-04-20 12:31:03 +0900

    enter 사용 가능

--- src/App.js
+++ src/App.js
@@ -11,13 +11,36 @@ const imageEndpoint = "https://printer.tathata.art/prints-new/image"
 
 function App() {
  const [text, setText] = useState("")
+ const [imageRender, setImageRender] = useState(null)
+ const [image, setImage] = useState(null)
 
  const print = () => {
+  // console.log(text)
   axios
-   .post("https://printer.tathata.art/prints-new/text", {
+   .post(textEndpoint, {
     text: text,
    })
-   .then((res) => setText(""))
+   .then((res) => {
+    setText("")
+    setImageRender(null)
+    setImage(null)
+   })
+ }
+
+ const imgPrint = () => {
+  const formData = new FormData()
+  formData.append("image", image)
+  axios
+   .post(imageEndpoint, formData, {
+    headers: {
+     "Content-Type": "multipart/form-data",
+    },
+   })
+   .then((res) => {
+    setText("")
+    setImageRender(null)
+    setImage(null)
+   })
  }
 
  return (
@@ -29,24 +52,45 @@ function App() {
     <p className="title">언베일드 공식 프린터</p>
     <p className="description">(이 프린터는 무료로 해줍니다 🤑)</p>
     <div className="text-print-section">
-     <div className="text-subtitle">텍스트 출력</div>
-     <input
+     {/* <div className="text-subtitle">텍스트 출력</div> */}
+     <textarea
       className="text-input"
       datatype="text"
       value={text}
       onChange={(e) => {
        setText(e.target.value)
       }}
-      onKeyDown={() => {
-       if (window.event.keyCode === 13) {
-        print()
-       }
-      }}
       placeholder="please speak english please~"
-     ></input>
+     ></textarea>
+     <div className="button-wrapper">
+      <button onClick={() => print()}>인쇄</button>
+     </div>
     </div>
     {/* <div className="image-print-section">
      <div className="image-subtitle">이미지 출력</div>
+     <input
+      className="image-input"
+      type="file"
+      accept=".jpg, .png"
+      onChange={(e) => {
+       if (e.target.files[0]) {
+        const imgSrc = new FileReader()
+        imgSrc.readAsDataURL(e.target.files[0])
+        imgSrc.onloadend = () => {
+         setImageRender(imgSrc.result)
+        }
+        setImage(e.target.files[0])
+       } else {
+        setImageRender(null)
+       }
+      }}
+     ></input>
+    </div> */}
+    {/* <div className="image-render">
+     {imageRender ? <img className="selected-image" alt="현재 선택된 이미지" src={imageRender}></img> : <div className="selected-image"></div>}
+     <div>
+      <button onClick={(e) => imgPrint()}>이미지 출력하기</button>
+     </div>
     </div> */}
    </PageStyle>
   </div>
@@ -84,6 +128,7 @@ const PageStyle = styled.div`
   margin: 0.5rem;
   /* height: 20rem; */
   width: 20rem;
+  height: 30rem;
   resize: none;
   outline: none;
   padding: 0.2rem;
@@ -100,15 +145,42 @@ const PageStyle = styled.div`
  :-ms-input-placeholder {
   text-align: center;
  }
+ .image-print-section,
  .text-print-section {
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
+  button {
+   height: 2rem;
+   width: 3rem;
+   margin-bottom: 0.5rem;
+  }
  }
+ .button-wrapper {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+ }
+ .image-subtitle,
  .text-subtitle {
   font-family: "Black Han Sans", sans-serif;
   font-size: 1.5rem;
   margin-right: 0.5rem;
+ }
+ .selected-image {
+  /* position: absolute;
+  right: -6rem;
+  top: 50%; */
+  margin-top: 1rem;
+  max-height: 11rem;
+ }
+ .image-render {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
  }
 `
 
